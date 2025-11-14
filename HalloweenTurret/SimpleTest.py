fb511b4cc8da6cd9154843cac49463759735abcb
AJax_Exe <alyssajackson436@gmail.com>
2025-11-14 09:34:57 -0700

    Simple Test done

--- HalloweenTurret/SimpleTest.py
+++ HalloweenTurret/SimpleTest.py
@@ -18,7 +18,7 @@ def main():
         
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         
-        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+        faces = face_cascade.detectMultiScale(gray, 1.05, 8, minSize=(120,120))
        
         for (x, y, w, h) in faces:
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
