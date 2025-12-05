599bd7c7707c20c133bdfe8a8c0a1d59b4e0ee0f
AJax_Exe <alyssajackson436@gmail.com>
2025-12-05 10:59:58 -0700

    More Progress

--- HalloweenTurret/Examples and Test/ProjectSimulate.py
+++ HalloweenTurret/Examples and Test/ProjectSimulate.py
@@ -3,14 +3,6 @@ import time
 import numpy as np
 import cv2
 
-arduinoData = serial.Serial('/dev/ttyACM0', 9600)
-
-def send_coordinates_to_arduino(x, y, w, h):
-    # Convert the coordinates to a string and send it to Arduino
-    coordinates = f"{x},{y}\r"
-    arduinoData.write(coordinates.encode())
-    print(f"X{x}Y{y}\n")
-
 capture = cv2.VideoCapture(0) #Change the number for the camera that you are using, 0 is for the internal laptop camera, 1 is for an external webcam
 face_cascade = cv2.CascadeClassifier('HalloweenTurret/Haar Cascade Classifiers/haarcascade_frontalface_default.xml')
 
@@ -19,9 +11,17 @@ while True:
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.05, 8, minSize=(120,120))
+    forehead = int(h/3)
     for (x, y, w, h) in faces:
+        forehead = int(h/3)
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 5)
-        send_coordinates_to_arduino(x, y, w, h)
+
+        # Forehead Box and Label
+        cv2.rectangle(frame, (x,y), (x+w, y+forehead), (255,0,0), 2)
+        cv2.putText(frame, "Forehead", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255,0,0), 2)
+
+        # Laser Simulation
+        # cv2.circle(frame, )
 
     cv2.imshow('Video', frame)
 
